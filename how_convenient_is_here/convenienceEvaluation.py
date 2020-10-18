c0c5b9cf11afadd844b4e0a4436a6367a0fd121b
Peterorr Chien <b00501021@gmail.com>
2020-10-18 13:01:49 +0800

    used param query; applied autopep8 on scripts; removed unuse attributes

--- how_convenient_is_here/convenienceEvaluation.py
+++ how_convenient_is_here/convenienceEvaluation.py
@@ -12,31 +12,28 @@ import os
 import gmaps
 import gmaps.datasets
 import random
+
+
 class ConvenienceEvaluation():
     # fields_to_types
     search_fields = {
-                    "food": ["restaurant", "cafe"],
-                    "live": ["convenience_store", "supermarket"],
-                    "transportation": ["bus_station", "subway_station"],
-                    "entertainment": ["movie_theater", "gym"],
-                    "environment" : ["park", "school"]
+        "food": ["restaurant", "cafe"],
+        "live": ["convenience_store", "supermarket"],
+        "transportation": ["bus_station", "subway_station"],
+        "entertainment": ["movie_theater", "gym"],
+        "environment": ["park", "school"]
     }
 
     def __init__(self, address, key):
         # 定義實體屬性
-        self.address = address
-        self.key = key
-        self.gmap_client = googlemaps.Client(key = key)
-        address_prefix = "https://maps.googleapis.com/maps/api/geocode/json?address="
-        address_key =  "&key=" + self.key
-        address_url = address_prefix + self.address + address_key
-        re = requests.get(address_url)
-        # 中文url需要轉碼才能識别
+        self.gmap_client = googlemaps.Client(key=key)
+        payload = {"address": self.address, "key": self.key}
+        re = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params=payload)
         self.place_info = json.loads(re.text)
         self.latitude = self.place_info['results'][0]['geometry']['location']['lat']
         self.longitude = self.place_info['results'][0]['geometry']['location']['lng']
         self.places_table = pd.DataFrame()
-    
+
     def dist_calculator(self, place_lat, place_lon):
         # 算兩點之間的距離
         """
@@ -47,13 +44,15 @@ class ConvenienceEvaluation():
         # lon:longitude
         # convert decimal degrees to radians
         here_lat = self.latitude
-        here_lon = self.longitude 
-        here_lat, here_lon, place_lat, place_lon = map(radians, [here_lat, here_lon, place_lat, place_lon])
-        # haversine formula 
-        diff_lat = place_lat - here_lat 
+        here_lon = self.longitude
+        here_lat, here_lon, place_lat, place_lon = map(
+            radians, [here_lat, here_lon, place_lat, place_lon])
+        # haversine formula
+        diff_lat = place_lat - here_lat
         diff_lon = place_lon - here_lon
         # 圓心角 Central angle
-        angle = 2 * asin(sqrt(sin(diff_lat/2)**2 + cos(here_lon) * cos(place_lon) * sin(diff_lon/2)**2)) 
+        angle = 2 * asin(sqrt(sin(diff_lat/2)**2 + cos(here_lon)
+                              * cos(place_lon) * sin(diff_lon/2)**2))
         # Radius of earth in kilometers is 6371
         res = 6371 * angle * 1000
         res = round(res, 3)
@@ -77,52 +76,57 @@ class ConvenienceEvaluation():
         position = str(self.latitude) + "," + str(self.longitude)
         places_result = []
         for each_type in self.search_types:
-            places_info = self.gmap_client.places_nearby(location = position, radius = radius, type = each_type)
+            places_info = self.gmap_client.places_nearby(
+                location=position, radius=radius, type=each_type)
             places_result += places_info["results"]
             count = 0
             while (count < 10 and places_info.get("next_page_token")):
                 time.sleep(2)
-                places_info = self.gmap_client.places_nearby(page_token = places_info["next_page_token"])
-                places_result += places_info["results"]      
-        self.places_table = pd.DataFrame(places_result)    
-        geo_locate = self.places_table["geometry"].apply(lambda x: x["location"])
-        self.places_table["distance"] = geo_locate.apply(lambda x: self.dist_calculator(x["lat"], x["lng"]))
+                places_info = self.gmap_client.places_nearby(
+                    page_token=places_info["next_page_token"])
+                places_result += places_info["results"]
+        self.places_table = pd.DataFrame(places_result)
+        geo_locate = self.places_table["geometry"].apply(
+            lambda x: x["location"])
+        self.places_table["distance"] = geo_locate.apply(
+            lambda x: self.dist_calculator(x["lat"], x["lng"]))
         self.places_table["distance"].loc[self.places_table["distance"] < 1] = 1
         self.places_table["latitude"] = geo_locate.apply(lambda x: x["lat"])
         self.places_table["longitude"] = geo_locate.apply(lambda x: x["lng"])
         self.places_table["rating"] = self.places_table["rating"].fillna(0)
-        # self.places_table = self.places_table[["name", "distance", "vicinity", "rating", "latitude", "longitude"]]
-        
+
     def giving_weight(self,
-                w_restaurant = 'self.places_table["rating"] / self.places_table["distance"] ** 0.5',
-                w_cafe = 'self.places_table["rating"] / self.places_table["distance"] ** 0.5',
-                w_convenience_store = '2',
-                w_supermarket = '2',
-                w_bus_station = '2',
-                w_subway_station = '10',
-                w_movie_theater = '4',
-                w_gym = '3',
-                w_park = '3',
-                w_school = '2'):       
+                      w_restaurant='self.places_table["rating"] / self.places_table["distance"] ** 0.5',
+                      w_cafe='self.places_table["rating"] / self.places_table["distance"] ** 0.5',
+                      w_convenience_store='2',
+                      w_supermarket='2',
+                      w_bus_station='2',
+                      w_subway_station='10',
+                      w_movie_theater='4',
+                      w_gym='3',
+                      w_park='3',
+                      w_school='2'):
         self.grading_manual = {
-                    "restaurant": w_restaurant,
-                    "cafe": w_cafe,
-                    "convenience_store": w_convenience_store,
-                    "supermarket": w_supermarket,
-                    "bus_station": w_bus_station,
-                    "subway_station": w_subway_station,
-                    "movie_theater": w_movie_theater,
-                    "gym": w_gym,
-                    "park": w_park,
-                    "school": w_school    
+            "restaurant": w_restaurant,
+            "cafe": w_cafe,
+            "convenience_store": w_convenience_store,
+            "supermarket": w_supermarket,
+            "bus_station": w_bus_station,
+            "subway_station": w_subway_station,
+            "movie_theater": w_movie_theater,
+            "gym": w_gym,
+            "park": w_park,
+            "school": w_school
         }
-    
+
     def get_point(self):
         self.points = {}
         for _type in self.grading_manual:
-            _type_count = sum(self.places_table["types"].apply(lambda x: _type in x))
-            _type_point = round(sum(self.places_table["types"].apply(lambda x: _type in x) * eval(self.grading_manual[_type])), 2)
+            _type_count = sum(
+                self.places_table["types"].apply(lambda x: _type in x))
+            _type_point = round(sum(self.places_table["types"].apply(
+                lambda x: _type in x) * eval(self.grading_manual[_type])), 2)
             _type_point = _type_point if _type_point <= 10 else 10
-            self.points[_type] = {"count": _type_count, "point" : _type_point}
+            self.points[_type] = {"count": _type_count, "point": _type_point}
         self.points = pd.DataFrame.from_dict(self.points).T
         self.total = sum(self.points["point"])
