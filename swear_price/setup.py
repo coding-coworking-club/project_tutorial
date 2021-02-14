90f364cab7b1c98e6f2d808e9f35130bcb03ff78
Peterorr Chien <b00501021@gmail.com>
2021-02-14 12:52:42 +0800

    initial setting: pipenv for selenium, cookiecutte; cookiecutte for strcuturing project folders

---
+++ swear_price/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A web scraper project for investigating how much does it cost when you swear words ',
+    author='Shu-Ting',
+    license='MIT',
+)
