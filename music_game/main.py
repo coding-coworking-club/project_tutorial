aceb5110f1eb65041e542aafb4a97a4c9173e6b8
YC <yuchilin0515@gmail.com>
2021-09-12 16:58:47 +0800

    刪除測試的code & 註解目前無用的code

--- music_game/main.py
+++ music_game/main.py
@@ -39,20 +39,20 @@ class mainwin(tk.Tk):
 
 # 測試實體化的Theme class能否讓修改過的mainwin正常運作
 # 已確認可以運作
-class Theme(tk.Frame):
-    def __init__(self, theme_name):
-        super().__init__(self)
-        super().configure(self, width=600, height=400, bg='beige')
-        self.pack_propagate(0)
+# class Theme(tk.Frame):
+#     def __init__(self, theme_name):
+#         super().__init__(self)
+#         super().configure(self, width=600, height=400, bg='beige')
+#         self.pack_propagate(0)
 
-        tk.Label(self, text=theme_name[0], font=('微軟正黑體', 18, "bold"),height=2, bg="beige", fg="#264653").pack(side="top", fill="x", pady=5)
-        for song_name in theme_name[1]:
-            tk.Label(self, text= song_name, font=('微軟正黑體', 15), bg="beige").pack(padx=5, pady=5)
-        tk.Label(self, text="請輸入歌曲代碼：", font=('微軟正黑體', 12, 'bold'), bg="beige", fg="#264653").pack(padx=5, pady=5)
+#         tk.Label(self, text=theme_name[0], font=('微軟正黑體', 18, "bold"),height=2, bg="beige", fg="#264653").pack(side="top", fill="x", pady=5)
+#         for song_name in theme_name[1]:
+#             tk.Label(self, text= song_name, font=('微軟正黑體', 15), bg="beige").pack(padx=5, pady=5)
+#         tk.Label(self, text="請輸入歌曲代碼：", font=('微軟正黑體', 12, 'bold'), bg="beige", fg="#264653").pack(padx=5, pady=5)
         
-        tk.Button(self, text="確定", font=('微軟正黑體', 12, "bold"), relief ="groove",
-        command=print('1')).place(y=320, x=355)                 
-        # 隨便寫一個function代替master.switch_frame   
+#         tk.Button(self, text="確定", font=('微軟正黑體', 12, "bold"), relief ="groove",
+#         command=print('1')).place(y=320, x=355)                 
+#         隨便寫一個function代替master.switch_frame   
 
 class StartPage(tk.Frame):
     def __init__(self, master):#anchor=tk.CENTER, 
@@ -77,50 +77,20 @@ class RulePage(tk.Frame):
                  font=('微軟正黑體', 14), justify = tk.LEFT).pack(padx=5, pady=5)
         tk.Button(self, text="回到主畫面", font=('微軟正黑體', 12, "bold"), relief ="groove",
                   command=lambda: master.back2home).pack(anchor=tk.S, padx=5, pady=5)
-class Theme2(tk.Frame):
-    def __init__(self, master, theme_name):
-        super().__init__(master)
-        super().configure(width=600, height=400, bg='beige')
-        self.pack_propagate(0)
 
-        tk.Label(self, text=theme_name[0], font=('微軟正黑體', 18, "bold"),height=2, bg="beige", fg="#264653").pack(side="top", fill="x", pady=5)
-        for song_name in theme_name[1]:
-            tk.Label(self, text= song_name, font=('微軟正黑體', 15), bg="beige").pack(padx=5, pady=5)
-        tk.Label(self, text="請輸入歌曲代碼：", font=('微軟正黑體', 12, 'bold'), bg="beige", fg="#264653").pack(padx=5, pady=5)
-        
-        tk.Button(self, text="確定", font=('微軟正黑體', 12, "bold"), relief ="groove",command= lambda: master.switch_frame(StartPage)).place(y=320, x=355)
-                                                                                                            
-# 實體化
-# t1 = Theme(['讚讚主題',['a','b','c']])
-
-# class StartPage(tk.Frame):##E0FFFF
-#     def __init__(self, master):#anchor=tk.CENTER, 
-#         super().__init__(master, width=600, height=400)
-#         super().configure(bg='beige')
+# class Theme2(tk.Frame):
+#     def __init__(self, master, theme_name):
+#         super().__init__(master)
+#         super().configure(width=600, height=400, bg='beige')
 #         self.pack_propagate(0)
-#         tk.Label(self, text='百萬大歌星', background='beige', height=2, font=('微軟正黑體', 24, "bold")).place(x=220, y=90)
-#         tk.Button(self, text="遊戲規則", font=('微軟正黑體', 14, "bold"), relief ="groove", 
-#         # t1 = theme的實體
-#         command=lambda: master.switch_frame(t1)).place(x=338, y=220)
-# StartPage點選遊戲規則會跳轉到上面的t1頁面
 
-app = mainwin()
-app.title("百萬大歌星")
-app.geometry('600x400')
-app.configure(background='beige')
-# app.iconbitmap('RBmic.ico')
-app.resizable(width=0, height=0)
-app.mainloop()
-
-
-
-
-
-# ---------------------------------------------------------------
-# 如果theme 有用到master?
-# 動不了
-
-
+#         tk.Label(self, text=theme_name[0], font=('微軟正黑體', 18, "bold"),height=2, bg="beige", fg="#264653").pack(side="top", fill="x", pady=5)
+#         for song_name in theme_name[1]:
+#             tk.Label(self, text= song_name, font=('微軟正黑體', 15), bg="beige").pack(padx=5, pady=5)
+#         tk.Label(self, text="請輸入歌曲代碼：", font=('微軟正黑體', 12, 'bold'), bg="beige", fg="#264653").pack(padx=5, pady=5)
+        
+#         tk.Button(self, text="確定", font=('微軟正黑體', 12, "bold"), relief ="groove",command= lambda: master.switch_frame(StartPage)).place(y=320, x=355)
+                                                                                                            
 
 # StartPage點選遊戲規則會跳轉到上面的t2頁面
 
